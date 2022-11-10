43eb0399f4d79c23552d0a87c7b760a266c6e949
OopssSorry <96795888+OopssSorry@users.noreply.github.com>
2022-11-10 20:01:47 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,5 @@
 
-if tonumber(game.PlaceId) == 537413528 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/537413528.lua"))() 
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/0000000000.lua"))() 
+if tonumber(game.PlaceId) == 537413528 then loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/537413528.lua"))() 
+elseif tonumber(game.PlaceId) == 7180042682 then loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/7180042682.lua"))() 
+else loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/OopssHub/main/games/0000000000.lua"))() 
 end
